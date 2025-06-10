8bbc7c8f34de5237c72d1150fdd2f3c5f6f2899b
Ana Amarante <kacaamarante@hotmail.com>
2025-06-10 09:13:26 -0300

    Criar logica no javascript para verificar quando esta ou nao clicado o primeiro check, pegar dados

--- js/form.js
+++ js/form.js
@@ -2,7 +2,14 @@
 //class contato
 
 class contato {
-    
+    constructor(nome,sobrenome,email,cpf,telefone,contato){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
 }
 
 function Post(form) {
@@ -13,15 +20,23 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
-  
+    
+            if (data.nome != "" ) {
+                alert('Obrigado sr(a) ' + data.nome + ' os seus dados foram encaminhados com sucesso');
+            }
 }
 
-function Enviar() {
+document.getElementById("enviar_forms").disabled = true;
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+function TermoAceite(){
+    let checkAceite = document.getElementById("termo_aceite");
+    let elemento = document.getElementById('enviar_forms');
+    if (checkAceite.checked){
+        elemento.classList.remove('botao_desabilitado');
+        document.getElementById("enviar_forms").disabled = false;
     }
-
+    else{
+        elemento.classList.add('botao_desabilitado');
+        document.getElementById("enviar_forms").disabled = true;
+    }
 }
